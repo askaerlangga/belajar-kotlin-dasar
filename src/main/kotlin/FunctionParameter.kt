b6eeed653f0b8b655412335e80710db93597a139
Aska Erlangga <ae.askaerlangga@gmail.com>
2021-04-03 15:08:41 +0700

    Commit

--- src/main/kotlin/FunctionParameter.kt
+++ src/main/kotlin/FunctionParameter.kt
@@ -1,12 +1,12 @@
 fun namaLengkap(namaDepan:String,namaBelakang:String){
     println("Halo $namaDepan $namaBelakang")
 }
-fun menjumlahkan(angka1:Int,angka2:Int){
-    var total = angka1 + angka2
-    println(" Total = $angka1 + $angka2 = $total")
+fun menjumlahkan(angka1:Int,angka2:Int){ // Function dengan Parameter
+    val total = angka1 + angka2
+    println("Total = $angka1 + $angka2 = $total")
 }
 
 fun main() {
-    namaLengkap("Aska", "Erlangga")
+    namaLengkap("Aska", "Erlangga") // Memanggil function
     menjumlahkan(90,20)
 }
